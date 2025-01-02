163f56ae4b3e27c9ab0a0fcd5f9fc03ad31274ba
Covenant Okoroafor <Okoroaforcovenant99@gmail.com>
2025-01-02 17:04:46 +0100

    feat: read json files from the directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main(){
+
+    let _json_file = fs::read_to_string("data.json").expect("Error: No JSON file to read!!");   
+
+
+
 }
