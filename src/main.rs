8402026bbb5553118b04700ca775116de8669b26
Covenant Okoroafor <Okoroaforcovenant99@gmail.com>
2025-01-07 03:44:19 +0100

    feat: implement command-line arguments and basic error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::fs;
+use std::env;
 use json::JsonValue;
 
 fn main(){
-    let json_str = fs::read_to_string("data.json").expect("No file found");
+
+    let Some(json_path) = env::args().nth(1) else {panic!("No file to query")};
+    
+    let Some(query)= env::args().nth(2) else {panic!("No query to execute")};
+
+    let Ok(json_str) = fs::read_to_string(&json_path) else {panic!("No file named {json_path:?} exists")};
 
     let parsed = json::parse(&json_str.to_string());
 
     match parsed {
         Ok(JsonValue::Object(obj))=>{
-            if let Some(value) = obj.get("naive"){
+            if let Some(value) = obj.get(&query){
                 println!("Found the value {value:?}");
             }
         }
