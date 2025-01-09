3da7b77742b1ecb396ee856ab28dc52488609ceb
Covenant Okoroafor <Okoroaforcovenant99@gmail.com>
2025-01-09 10:54:51 +0100

    feat: add support for nested json queries

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
-use std::fs;
-use std::env;
 use json::JsonValue;
+use std::env;
+use std::fs;
 
-fn main(){
+fn main() {
+    let Some(json_path) = env::args().nth(1) else {
+        panic!("No file to query")
+    };
 
-    let Some(json_path) = env::args().nth(1) else {panic!("No file to query")};
-    
-    let Some(query)= env::args().nth(2) else {panic!("No query to execute")};
+    let Some(query) = env::args().nth(2) else {
+        panic!("No query to execute")
+    };
 
-    let Ok(json_str) = fs::read_to_string(&json_path) else {panic!("No file named {json_path:?} exists")};
+    let queries: Vec<&str> = query.split('.').collect();
+
+    let Ok(json_str) = fs::read_to_string(&json_path) else {
+        panic!("No file named {json_path:?} exists")
+    };
 
     let parsed = json::parse(&json_str.to_string());
 
     match parsed {
-        Ok(JsonValue::Object(obj))=>{
-            if let Some(value) = obj.get(&query){
-                println!("Found the value {value:?}");
+        Ok(JsonValue::Object(obj)) => {
+            let obj = &JsonValue::Object(obj);
+            let mut value: Option<&JsonValue> = Some(obj);
+
+            for query_part in queries {
+                value = match value {
+                    Some(JsonValue::Object(obj)) => obj.get(query_part),
+                    _ => {
+                        break;
+                    }
+                };
             }
+
+            if let Some(val) = value {
+                println!("The value of the query is {val:#}")
+            };
         }
-        Ok(_)=> println!("Root is not an object!"),
-        Err(err)=>println!("The operation failed with the error {err:?}"),   
-    
+        Ok(_) => println!("Root is not an object!"),
+        Err(err) => println!("The operation failed with the error {err:?}"),
     }
 }
